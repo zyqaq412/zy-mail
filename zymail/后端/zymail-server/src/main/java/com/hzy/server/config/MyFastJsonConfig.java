ea923ea885c9fa2f288885ec5d94f1ee0552b807
zyqaq412 <2791517764@qq.com>
2023-09-24 23:59:31 +0800

    zymail-server: 定时邮件功能 ： fastJson 默认时间格式

---
+++ zymail/后端/zymail-server/src/main/java/com/hzy/server/config/MyFastJsonConfig.java
@@ -0,0 +1,25 @@
+package com.hzy.server.config;
+
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @title: FastJsonConfig
+ * @Author zxwyhzy
+ * @Date: 2023/9/24 23:35
+ * @Version 1.0
+ */
+@Configuration
+public class MyFastJsonConfig {
+
+    @Bean
+    public FastJsonHttpMessageConverter fastJsonHttpMessageConverter() {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        converter.setFastJsonConfig(config);
+        return converter;
+    }
+}
