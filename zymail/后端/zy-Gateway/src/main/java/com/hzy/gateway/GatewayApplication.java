45957d11c241e0939559b0743a66fc8d69ad0602
zyqaq412 <2791517764@qq.com>
2023-10-27 13:09:17 +0800

    Gateway- 创建网关实现邮件管理平台的负载均衡

---
+++ zymail/后端/zy-Gateway/src/main/java/com/hzy/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.hzy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @title: GatewayApplication
+ * @Author zxwyhzy
+ * @Date: 2023/10/27 12:31
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
